9a2b84d058563388d5146ff42d7f7631005287f6
Jorge <jorgeortiz1982@hotmail.com>
2025-12-23 19:02:02 -0500

    Actualizar PWA con HomePage y BLE

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BluetoothLe } from '@capacitor-community/bluetooth-le';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule] // ← esto es lo clave
 })
 export class HomePage {
+
+  temperatura: string = '';
+
   constructor() {}
+
+  async scanAndConnect() {
+    await BluetoothLe.initialize();
+
+    const devices = await BluetoothLe.requestDevice({
+      services: [],
+      optionalServices: [],
+      namePrefix: 'HM-10'
+    });
+
+    if (devices) {
+      const deviceId = devices.deviceId;
+
+      await BluetoothLe.connect({ deviceId });
+
+      await BluetoothLe.startNotifications({
+        deviceId,
+        service: 'FFE0',
+        characteristic: 'FFE1'
+      }).then((result: any) => {
+        const data = new TextDecoder().decode(result.value);
+        this.temperatura = data;
+        console.log('Temperatura:', data);
+      });
+    }
+  }
 }
